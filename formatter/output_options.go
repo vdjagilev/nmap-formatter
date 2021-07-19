699641a8af894f64f1c8b6b7ac12a20651e1856d
vdjagilev <2762286+vdjagilev@users.noreply.github.com>
2021-07-19 03:36:31 +0300

    #8: Add skip options to OutputOptions struct

--- formatter/output_options.go
+++ formatter/output_options.go
@@ -6,4 +6,12 @@ type OutputOptions struct {
 	SkipDownHosts bool
 	// JSONPrettyPrint defines if JSON output would be pretty-printed (human-readable) or not (machine readable)
 	JSONPrettyPrint bool
+	// SkipSummary skips general summary for HTML & Markdown formats
+	SkipSummary bool
+	// SkipTraceroute skips traceroute information for HTML & Markdown formats
+	SkipTraceroute bool
+	// SkipMetrics skips metrics related data for HTML & Markdown formats
+	SkipMetrics bool
+	// SkipPortScripts skips port scripts information for HTML & Markdown formats
+	SkipPortScripts bool
 }
