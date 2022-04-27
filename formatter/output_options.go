598e5ac277db08836dfb76912945494ed203fa92
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2022-04-27 15:58:34 +0300

    Remove commented out code

--- formatter/output_options.go
+++ formatter/output_options.go
@@ -2,26 +2,10 @@ package formatter
 
 // OutputOptions describes various output options for nmap formatter
 type OutputOptions struct {
-	HTMLOptions HTMLOutputOptions
-
+	HTMLOptions     HTMLOutputOptions
 	MarkdownOptions MarkdownOutputOptions
-
-	JSONOptions JSONOutputOptions
-
-	CSVOptions CSVOutputOptions
-
-	// The hosts that are down wont be displayed in the TOC
-	// SkipDownHosts bool
-	// JSONPrettyPrint defines if JSON output would be pretty-printed (human-readable) or not (machine readable)
-	//JSONPrettyPrint bool
-	// SkipSummary skips general summary for HTML & Markdown
-	// SkipSummary bool
-	// SkipTraceroute skips traceroute information for HTML & Markdown
-	// SkipTraceroute bool
-	// SkipMetrics skips metrics related data for HTML
-	// SkipMetrics bool
-	// SkipPortScripts skips port scripts information for HTML
-	// SkipPortScripts bool
+	JSONOptions     JSONOutputOptions
+	CSVOptions      CSVOutputOptions
 }
 
 type HTMLOutputOptions struct {
