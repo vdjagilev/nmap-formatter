9c143a82248190383b83d0b72c196311954ec391
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2022-04-28 16:06:41 +0300

    #77: Possibility to read xml content from stdin (#92)
    * #77: Possibility to read xml content from stdin
    
    * Remove commented out code
    
    * Remove commented out code

--- formatter/config.go
+++ formatter/config.go
@@ -8,10 +8,10 @@ import (
 // where output will be delivered, desired output format, input file path, output file path
 // and different output options
 type Config struct {
-	Writer        io.Writer
-	OutputFormat  OutputFormat
-	InputFile     InputFile
-	OutputFile    OutputFile
-	OutputOptions OutputOptions
-	ShowVersion   bool
+	Writer          io.Writer
+	OutputFormat    OutputFormat
+	InputFileConfig InputFileConfig
+	OutputFile      OutputFile
+	OutputOptions   OutputOptions
+	ShowVersion     bool
 }
--- formatter/file.go
+++ formatter/file.go
@@ -1,7 +1,42 @@
 package formatter
 
+import (
+	"bufio"
+	"errors"
+	"io"
+	"os"
+)
+
 // OutputFile describes output file name (full path)
 type OutputFile string
 
 // InputFile describes input file (nmap XML full path)
 type InputFile string
+
+type InputFileConfig struct {
+	Path    string
+	IsStdin bool
+	Source  io.Reader
+}
+
+// ReadContents reads content from stdin or provided file-path
+func (i *InputFileConfig) ReadContents() ([]byte, error) {
+	var err error
+	var content []byte
+	if i.Source == nil {
+		return nil, errors.New("no reading source is defined")
+	}
+	scanner := bufio.NewScanner(i.Source)
+	for scanner.Scan() {
+		content = append(content, scanner.Bytes()...)
+	}
+	err = scanner.Err()
+	return content, err
+}
+
+// ExistsOpen tries to open a file for reading, returning an error if it fails
+func (i *InputFileConfig) ExistsOpen() error {
+	f, err := os.Open(i.Path)
+	f.Close()
+	return err
+}
