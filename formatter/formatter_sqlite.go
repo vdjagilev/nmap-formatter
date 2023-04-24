5f5227a3f3344b4df0d8a9c01fbbe0ac7632f8db
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2023-04-24 21:41:55 +0300

    feat: implement rest of the queries

--- formatter/formatter_sqlite.go
+++ formatter/formatter_sqlite.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 
 	// Written this way to avoid automatic removal by text editor
 	_ "github.com/mattn/go-sqlite3"
@@ -46,11 +47,10 @@ func (f *SqliteFormatter) Format(td *TemplateData, templateContent string) error
 		}
 	}
 
-	// TODO: Transaction
-	//	tx, err := db.Begin()
-	//	if err != nil {
-	//		panic(err)
-	//	}
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
 
 	scanID, err := f.insertScan(db, &td.NMAPRun)
 	if err != nil {
@@ -58,6 +58,130 @@ func (f *SqliteFormatter) Format(td *TemplateData, templateContent string) error
 	}
 
 	log.Printf("New scan with ID (%d) is inserted", scanID)
+
+	for _, host := range td.NMAPRun.Host {
+		hostID, err := f.insertReturnID(
+			db,
+			insertHostsSQL,
+			scanID,
+			"TODO",
+			"TODO",
+			host.StartTime,
+			host.EndTime,
+			host.Status.State,
+			host.Status.Reason,
+			host.Uptime.Seconds,
+			host.Uptime.LastBoot,
+			host.Distance.Value,
+			host.TCPSequence.Index,
+			host.TCPSequence.Difficulty,
+			host.TCPSequence.Values,
+			host.IPIDSequence.Class,
+			host.IPIDSequence.Values,
+			host.TCPTSSequence.Class,
+			host.TCPTSSequence.Values,
+			host.Trace.Port,
+			host.Trace.Protocol,
+			host.Status.State,
+		)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		log.Printf("New hostID (%d) is created", hostID)
+
+		err = f.insertHostTracesHops(db, hostID, host.Trace.Hops)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		log.Printf("Host trace hops are inserted for host ID (%d)", hostID)
+
+		err = f.insertHostAddresses(db, hostID, host.HostAddress)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		log.Printf("Host addresses are inserted for host ID (%d)", hostID)
+
+		err = f.insertHostNames(db, hostID, &host.HostNames)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		log.Printf("Host names are inserted for host ID (%d)", hostID)
+
+		osID, err := f.insertOSRecords(db, hostID, &host.OS)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		log.Printf("OS record is inserted ID (%d)", osID)
+
+		err = f.insertOSPortUsed(db, osID, host.OS.OSPortUsed)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		log.Printf("Insert OS port used for os ID (%d)", osID)
+
+		err = f.insertOSMatch(db, osID, host.OS.OSMatch)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		portInsert, err := db.Prepare(insertPortsSQL)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		for _, portInsertRecord := range host.Port {
+			result, err := portInsert.Exec(
+				hostID,
+				portInsertRecord.PortID,
+				portInsertRecord.State.State,
+				portInsertRecord.State.Reason,
+				portInsertRecord.State.ReasonTTL,
+				portInsertRecord.Service.Name,
+				portInsertRecord.Service.Product,
+				portInsertRecord.Service.Version,
+				portInsertRecord.Service.ExtraInfo,
+				portInsertRecord.Service.Method,
+				portInsertRecord.Service.Conf,
+				strings.Join(portInsertRecord.Service.CPE, sqliteStringDelimiter),
+			)
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+
+			portID, err := result.LastInsertId()
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+
+			err = f.insertPortScripts(db, portID, portInsertRecord.Script)
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+		}
+
+		portInsert.Close()
+	}
+
+	if err != nil {
+		tx.Commit()
+	}
 
 	return err
 }
