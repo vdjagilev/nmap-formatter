f7633699300b2cae606e1788eea46fa00d293126
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2023-05-13 01:01:03 +0300

    refactor: implement unit-tests

---
+++ formatter/formatter_sqlite_test.go
@@ -0,0 +1,191 @@
+package formatter
+
+import (
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestSqliteFormatter_Format(t *testing.T) {
+	const DBDSN = "file::memory:?cache=shared"
+	var config = &Config{
+		OutputOptions: OutputOptions{
+			SqliteOutputOptions: SqliteOutputOptions{
+				DSN: DBDSN,
+			},
+		},
+		CurrentVersion: "1",
+	}
+	type fields struct {
+		config *Config
+	}
+	type args struct {
+		td              *TemplateData
+		templateContent string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "No data",
+			fields: fields{
+				config,
+			},
+			args: args{
+				td: &TemplateData{
+					NMAPRun: NMAPRun{},
+				},
+				templateContent: "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "1 scan, 2 hosts, 2 ports",
+			fields: fields{
+				config,
+			},
+			args: args{
+				td: &TemplateData{
+					NMAPRun: NMAPRun{
+						Scanner: "nmap",
+						Host: []Host{
+							{
+								StartTime: 0,
+								EndTime:   0,
+								Port: []Port{
+									{
+										Protocol: "tcp",
+										PortID:   80,
+										State:    PortState{},
+										Service:  PortService{},
+										Script: []Script{
+											{
+												ID:     "http",
+												Output: "abc",
+											},
+										},
+									}, {
+										Protocol: "tcp",
+										PortID:   443,
+										State:    PortState{},
+										Service:  PortService{},
+										Script: []Script{
+											{
+												ID:     "https",
+												Output: "abc",
+											},
+										},
+									},
+								},
+								HostAddress: []HostAddress{
+									{
+										Address:     "192.168.1.1",
+										AddressType: "ipv4",
+									},
+									{
+										Address:     "192.168.2.1",
+										AddressType: "ipv4",
+									},
+								},
+								HostNames: HostNames{
+									HostName: []HostName{
+										{
+											Name: "example.com",
+											Type: "A",
+										},
+										{
+											Name: "www.example.com",
+											Type: "AA",
+										},
+									},
+								},
+								Status:        HostStatus{},
+								OS:            OS{},
+								Trace:         Trace{},
+								Uptime:        Uptime{},
+								Distance:      Distance{},
+								TCPSequence:   TCPSequence{},
+								IPIDSequence:  IPIDSequence{},
+								TCPTSSequence: TCPTSSequence{},
+							},
+							{
+								StartTime: 0,
+								EndTime:   0,
+								Port: []Port{
+									{
+										Protocol: "tcp",
+										PortID:   80,
+										State:    PortState{},
+										Service:  PortService{},
+										Script: []Script{
+											{
+												ID:     "http",
+												Output: "abc",
+											},
+										},
+									}, {
+										Protocol: "tcp",
+										PortID:   443,
+										State:    PortState{},
+										Service:  PortService{},
+										Script: []Script{
+											{
+												ID:     "https",
+												Output: "abc",
+											},
+										},
+									},
+								},
+								HostAddress: []HostAddress{
+									{
+										Address:     "192.168.1.1",
+										AddressType: "ipv4",
+									},
+									{
+										Address:     "192.168.2.1",
+										AddressType: "ipv4",
+									},
+								},
+								HostNames: HostNames{
+									HostName: []HostName{
+										{
+											Name: "example.com",
+											Type: "A",
+										},
+										{
+											Name: "www.example.com",
+											Type: "AA",
+										},
+									},
+								},
+								Status:        HostStatus{},
+								OS:            OS{},
+								Trace:         Trace{},
+								Uptime:        Uptime{},
+								Distance:      Distance{},
+								TCPSequence:   TCPSequence{},
+								IPIDSequence:  IPIDSequence{},
+								TCPTSSequence: TCPTSSequence{},
+							},
+						},
+					},
+				},
+				templateContent: "",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &SqliteFormatter{
+				config: tt.fields.config,
+			}
+			if err := f.Format(tt.args.td, tt.args.templateContent); (err != nil) != tt.wantErr {
+				t.Errorf("SqliteFormatter.Format() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
