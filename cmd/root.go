d7a8dcb708d612808d05316194564439566ce771
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2023-11-03 19:15:40 +0200

    bump version to 2.1.5

--- cmd/root.go
+++ cmd/root.go
@@ -45,7 +45,7 @@ var config = formatter.Config{
 }
 
 // VERSION is describing current version of the nmap-formatter
-const VERSION string = "2.1.4"
+const VERSION string = "2.1.5"
 
 var workflow formatter.Workflow
 
