44dcebde90aa59a81c358b6531072e79954680d4
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2024-02-21 23:34:46 +0200

    refactor: split into smaller functions

--- formatter/formatter_excel.go
+++ formatter/formatter_excel.go
@@ -11,8 +11,24 @@ type ExcelFormatter struct {
 	config *Config
 }
 
+type CellData struct {
+	sheetName string
+	style     int
+	file      *excelize.File
+}
+
+func (cd *CellData) writeCell(cell string, value string) error {
+	err := cd.file.SetCellValue(cd.sheetName, cell, value)
+	if err != nil {
+		return err
+	}
+
+	return cd.file.SetCellStyle(cd.sheetName, cell, cell, cd.style)
+}
+
 // Format the data to Excel and output it to an Excel file
 func (f *ExcelFormatter) Format(td *TemplateData, templateContent string) (err error) {
+	// TODO: Write unit tests
 	file := excelize.NewFile()
 	sheetName := "Sheet1"
 
@@ -24,63 +40,100 @@ func (f *ExcelFormatter) Format(td *TemplateData, templateContent string) (err e
 		return err
 	}
 
+	// Reusable cell data struct for writing cells
+	cd := &CellData{
+		sheetName: sheetName,
+		style:     style,
+		file:      file,
+	}
+
 	// Set column headers with titles
-	err = f.writeHeaders(sheetName, file, style)
+	err = f.writeHeaders(cd)
 	if err != nil {
 		return err
 	}
 
-	row := 2 // Start from row 2 for data
-
-	for i := range td.NMAPRun.Host {
-		host := &td.NMAPRun.Host[i]
-		// Skipping hosts that are down
-		if td.OutputOptions.ExcelOptions.SkipDownHosts && host.Status.State != "up" {
-			continue
-		}
-		address := fmt.Sprintf("%s (%s)", host.JoinedAddresses("/"), host.JoinedHostNames("/"))
-
-		// Set the IP/Host value
-		cell := fmt.Sprintf("A%d", row)
-		file.SetCellValue(sheetName, cell, address)
-		file.SetCellStyle(sheetName, cell, cell, style)
-
-		startRow := row // Remember the start row for this host
-
-		for j := range host.Port {
-			port := &host.Port[j]
-			col := 'B' // Start from column B for Services
-
-			// Set the Service value
-			cell = fmt.Sprintf("%c%d", col, row)
-			file.SetCellValue(sheetName, cell, fmt.Sprintf("%d/%s %s", port.PortID, port.Protocol, port.Service.Name))
-			file.SetCellStyle(sheetName, cell, cell, style)
-			row++ // Increment row for next service
-		}
-
-		// Merge cells in the IP/Host column for this host
-		if row > startRow+1 { // Only merge if there's more than one service
-			file.MergeCell(sheetName, fmt.Sprintf("A%d", startRow), fmt.Sprintf("A%d", row-1))
-		}
+	err = f.writeHostRows(td.NMAPRun.Host, cd)
+	if err != nil {
+		return err
 	}
 
 	return file.Write(f.config.Writer, excelize.Options{})
 }
 
-func (f *ExcelFormatter) writeHeaders(sheetName string, file *excelize.File, style int) error {
-	err := file.SetCellValue(sheetName, "A1", "IP/Host")
+func (f *ExcelFormatter) writeHostRows(h []Host, cd *CellData) error {
+	var err error
+	row := 2 // Start from row 2 for data
+
+	for i := range h {
+		host := h[i]
+
+		// Skipping hosts that are down
+		if f.config.OutputOptions.ExcelOptions.SkipDownHosts && host.Status.State != "up" {
+			continue
+		}
+
+		address := fmt.Sprintf(
+			"%s (%s)",
+			host.JoinedAddresses("/"),
+			host.JoinedHostNames("/"),
+		)
+
+		// Set the IP/Host value
+		cell := fmt.Sprintf("A%d", row)
+		err = cd.writeCell(cell, address)
+		if err != nil {
+			return err
+		}
+
+		startRow := row // Remember the start row for this host
+		err = f.writePorts(host.Port, cd, &row)
+		if err != nil {
+			return err
+		}
+
+		// Merge cells in the IP/Host column for this host
+		if row > startRow+1 {
+			err = cd.file.MergeCell(
+				cd.sheetName,
+				fmt.Sprintf("A%d", startRow),
+				fmt.Sprintf("A%d", row-1),
+			)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return err
+}
+
+func (f *ExcelFormatter) writePorts(p []Port, cd *CellData, row *int) error {
+	var err error
+
+	for i := range p {
+		port := p[i]
+
+		// Set the Service value for column B for services
+		err = cd.writeCell(
+			fmt.Sprintf("%c%d", 'B', *row),
+			fmt.Sprintf("%d/%s %s", port.PortID, port.Protocol, port.Service.Name),
+		)
+		if err != nil {
+			return err
+		}
+		*row++
+	}
+	return err
+}
+
+func (f *ExcelFormatter) writeHeaders(cd *CellData) error {
+	err := cd.writeCell("A1", "IP/Host")
 	if err != nil {
 		return err
 	}
-	err = file.SetCellValue(sheetName, "B1", "Services")
-	if err != nil {
-		return err
-	}
-	err = file.SetCellStyle(sheetName, "A1", "A1", style)
-	if err != nil {
-		return err
-	}
-	return file.SetCellStyle(sheetName, "B1", "B1", style)
+
+	return cd.writeCell("B1", "Services")
 }
 
 func (f *ExcelFormatter) defaultTemplateContent() string {
