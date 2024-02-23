b428bf88fb8014486fa4221adf12448d3473188a
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2024-02-23 20:29:15 +0200

    tests: excel: more test cases

--- formatter/formatter_excel_test.go
+++ formatter/formatter_excel_test.go
@@ -42,6 +42,85 @@ func TestExcelFormatter_Format(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "Test ExcelFormatter.Format Basic 3 hosts",
+			f: &ExcelFormatter{
+				config: &Config{
+					Writer: writer,
+				},
+			},
+			args: args{
+				td: &TemplateData{
+					NMAPRun: NMAPRun{
+						Host: []Host{
+							{
+								StartTime: 0,
+								EndTime:   0,
+								Port:      []Port{},
+								HostAddress: []HostAddress{
+									{
+										Address: "192.168.1.1",
+									},
+								},
+								HostNames: HostNames{},
+								Status: HostStatus{
+									State: "up",
+								},
+								OS:            OS{},
+								Trace:         Trace{},
+								Uptime:        Uptime{},
+								Distance:      Distance{},
+								TCPSequence:   TCPSequence{},
+								IPIDSequence:  IPIDSequence{},
+								TCPTSSequence: TCPTSSequence{},
+							},
+							{
+								StartTime: 0,
+								EndTime:   0,
+								Port:      []Port{},
+								HostAddress: []HostAddress{
+									{
+										Address: "192.168.1.2",
+									},
+								},
+								HostNames: HostNames{},
+								Status: HostStatus{
+									State: "down",
+								},
+								OS:            OS{},
+								Trace:         Trace{},
+								Uptime:        Uptime{},
+								Distance:      Distance{},
+								TCPSequence:   TCPSequence{},
+								IPIDSequence:  IPIDSequence{},
+								TCPTSSequence: TCPTSSequence{},
+							},
+							{
+								StartTime: 0,
+								EndTime:   0,
+								Port:      []Port{},
+								HostAddress: []HostAddress{
+									{
+										Address: "192.168.1.3",
+									},
+								},
+								HostNames: HostNames{},
+								Status: HostStatus{
+									State: "up",
+								},
+								OS:            OS{},
+								Trace:         Trace{},
+								Uptime:        Uptime{},
+								Distance:      Distance{},
+								TCPSequence:   TCPSequence{},
+								IPIDSequence:  IPIDSequence{},
+								TCPTSSequence: TCPTSSequence{},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
